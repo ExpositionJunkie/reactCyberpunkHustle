f76f6370cf0f1d7fb668b952e9726bbcd815395c
Roz <rosalindavalbrecht@gmail.com>
2021-05-13 21:21:27 -0700

    Very broken, need to implement build script for scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Jumbotron, Container } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+              <div className="pageHeader">
+                <Jumbotron fluid>
+                  <Container fluid>
+                    <h1 className="display-3">Cyberpunk Hustle</h1>
+                  </Container>
+                </Jumbotron>
+              </div>
+            </div>
+        );
+    }
 }
 
 export default App;
